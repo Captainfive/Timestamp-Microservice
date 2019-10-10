37d960764723f6e1d4e38b377b89755d1f6bcf84
Captainfive <vinou_montes@hotmail.fr>
2019-10-10 09:46:50 +0200

    chore(server): adding comments

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 // where your node app starts
 
-// init project
+// require third part dependencies and declare express app
 const express = require("express");
 const app = express();
 
@@ -14,18 +14,19 @@ app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 20
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get("/api/hello", function(req, res) {
+app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
 // API Project: Timestamp Microservice
 /**
  * @function isValidDate
+ * @description permit to identify if the argument to pass to the function is a javascript
  * @param {*} date to test
  * @returns {!boolean}
  */
@@ -33,20 +34,22 @@ function isValidDate(date) {
   return date instanceof Date && !isNaN(date);
 }
 
-app.get("short-alvarezsaurus/api/timestamp/:date_string?", async (req, res) => {
+
+// This route makes it possible to check if the date pass in parameters of the request is valid
+app.get("short-alvarezsaurus/api/timestamp/:date_string?", async(req, res) => {
   let dateReceived = req.params["date_string"];
 
   if (!Reflect.has(req.params, ["date_string"])) {
     dateReceived = Date.now();
   } else if (!isValidDate(new Date(dateReceived))) {
-//Express send
-    res.json({ error: "Invalid Date" });
-//Polka send
+    //Express send
+    res.status(400).json({ error: "Invalid Date" });
+    //Polka send
     // send(res, 400, { error: "Invalid Date" });
   }
 
   dateReceived = new Date(dateReceived);
-//Express send
+  //Express send
   res.json({
     unix: dateReceived.getTime(),
     utc: dateReceived.toUTCString()
@@ -59,6 +62,6 @@ app.get("short-alvarezsaurus/api/timestamp/:date_string?", async (req, res) => {
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
